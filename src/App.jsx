f78af55390d50c83578e7e8c4cfc80bf58fb654d
JayashTripathy <jayashtripathy371@gmail.com>
2023-06-18 12:34:58 +0530

    added some content

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ function App() {
 
 
   return (
-   
-      <div className="h-full w-full overflow-x-hidden">
 
-        <BackTop />
-        <section className=' bg-gray-900 text-white'>Scroll Down ⬇</section>
-        <section className=' bg-indigo-500  text-white'>Click on bottom right button ⬆️</section>
-        <section className=' bg-gray-900 text-white'>yeah! thats it.😉</section>
-        <section className=' bg-indigo-500 text-white'>yeah! thats it.😉</section>
+    <div className="h-full w-full overflow-x-hidden">
 
-      </div>
- 
+      <BackTop />
+      <section className=' bg-gray-900 text-white'>Scroll Down ⬇</section>
+      <section className=' bg-indigo-500  text-white'>Click on bottom right button ⬆️</section>
+      <section className=' bg-gray-900 text-white'>yeah! thats it.😉</section>
+      <section className=' bg-indigo-500 text-white'>
+        If you liked this, give it a star 🌟 on &nbsp;
+        <a href="https://github.com/JayashTripathy" target="_blank" className='bg-black p-1  px-4 rounded'>
+         Github
+        </a>
+      </section>
+
+    </div>
+
   )
 }
 
